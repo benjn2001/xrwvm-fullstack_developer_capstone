2726a4c9ca5448551c718e602ea1b87612786333
benjn2001 <benjn2001@gmail.com>
2024-04-10 15:57:44 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -11,10 +12,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -37,10 +37,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
+            validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
