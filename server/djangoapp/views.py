e28aeb35e7d8bc8955f95c8a90968002c614a713
benjn2001 <benjn2001@gmail.com>
2024-04-10 18:18:29 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -46,9 +46,8 @@ def registration(request):
     except User.DoesNotExist:
         logger.debug("{} is a new user".format(username))
     if not username_exist:
-        user = User.objects.create_user(username=username,
-                                      first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
+        user = User.objects.create_user(
+            username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
