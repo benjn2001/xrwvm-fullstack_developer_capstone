e26a87fbbeea390be3054174f1d649ba243ce4ee
benjn2001 <benjn2001@gmail.com>
2024-04-10 17:06:15 -0400

    Adding temporary changes to Github

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,19 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost', 
-    'https://benjn2001-8000.theiadockernext-1-labs-prod- '+
+    'https://benjn2001-8000.theiadockernext-1-labs-prod-' +
     'theiak8s-4-tor01.proxy.cognitiveclass.ai' 
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://benjn2001-8000.theiadockernext-1-labs-prod- '+
+    'https://benjn2001-8000.theiadockernext-1-labs-prod-' +
     'theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
@@ -101,8 +100,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation. ' +
-        'UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -151,4 +149,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
