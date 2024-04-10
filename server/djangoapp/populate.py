50fa2cc7b5718441779993b45eda38c93814a9f7
benjn2001 <benjn2001@gmail.com>
2024-04-10 17:30:12 -0400

    Adding temporary changes to Github

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,6 +1,5 @@
 from .models import CarMake, CarModel
 
-
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -12,8 +11,12 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
-            CarMake.objects.create(name=data['name'], description=data['description'])
+            CarMake.objects.create(
+                name=data['name'],
+                description=data['description']
+            )
         )
+    
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
         {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
