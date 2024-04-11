4753a8b31d8b465c3e0c6d1cbcb883b194a94b59
benjn2001 <benjn2001@gmail.com>
2024-04-10 21:04:36 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,115 +1,28 @@
-from django.contrib.auth.models import User
-from django.contrib.auth import logout, login, authenticate
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
-from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
-import logging
-import json
+# Uncomment the imports before you add the code
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
+app_name = 'djangoapp'
+urlpatterns = [
+    # # path for registration
+    path(route='registration', view=views.registration, name='registration'),
+    path(route='register', view=views.registration, name='register'),
+    # path for login
+    
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    
 
+    # path for dealer reviews view
 
-@csrf_exempt
-def login_user(request):
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    user = authenticate(username=username, password=password)
-    data = {"userName": username}
-    if user is not None:
-        login(request, user)
-        data["status"] = "Authenticated"
-    return JsonResponse(data)
+    # path for add a review view
 
-
-def logout_request(request):
-    logout(request)
-    data = {"userName": ""}
-    return JsonResponse(data)
-
-
-@csrf_exempt
-def registration(request):
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    username_exist = False
-    # email_exist = False  # Commented out because it's not used
-    try:
-        User.objects.get(username=username)
-        username_exist = True
-    except User.DoesNotExist:
-        logger.debug(f"{username} is a new user")
-    if not username_exist:
-        user = User.objects.create_user(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-            email=email
-        )
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-        return JsonResponse(data)
-    else:
-        data = {"userName": username, "error": "Already Registered"}
-        return JsonResponse(data)
-
-
-def get_dealerships(request, state="All"):
-    endpoint = "/fetchDealers" if state == "All" else f"/fetchDealers/{state}"
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
-
-
-def get_dealer_reviews(request, dealer_id):
-    if dealer_id:
-        endpoint = f"/fetchReviews/dealer/{dealer_id}"
-        reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
-
-def get_dealer_details(request, dealer_id):
-    if dealer_id:
-        endpoint = f"/fetchDealer/{dealer_id}"
-        dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
-
-def add_review(request):
-    if not request.user.is_anonymous:
-        data = json.loads(request.body)
-        try:
-            post_review(data)
-            return JsonResponse({"status": 200})
-        except Exception:
-            return JsonResponse({"status": 401, "message":
-                                "Error in posting review"})
-    else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
-
-
-def get_cars(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if count == 0:
-        initiate()
-
-    car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake":
-            car_model.car_make.name} for car_model in car_models]
-    return JsonResponse({"CarModels": cars})
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
