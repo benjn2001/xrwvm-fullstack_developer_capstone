aef71961ccac35b875846adcff694fffadd8c9cd
benjn2001 <benjn2001@gmail.com>
2024-04-11 14:34:13 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
@@ -27,6 +28,7 @@ def login_user(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
+
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
@@ -45,17 +47,18 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    try: 
+    try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except Exception: 
+    except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
     # If it is a new user
-    if not username_exist: 
+    if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name,
+            last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -68,7 +71,7 @@ def registration(request):
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
-    else: 
+    else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
@@ -77,21 +80,21 @@ def get_dealerships(request, state="All"):
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if (dealer_id): 
+    if (dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
-        for review_detail in reviews: 
+        for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
-    else: 
+    else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if (dealer_id): 
+    if (dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
@@ -100,26 +103,28 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
-        try: 
+        try:
             response = post_review(data)
-            return JsonResponse({"status":200})
+            return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
-    else: 
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+            return JsonResponse({"status": 401,"message":
+                "Error in posting review"})
+    else:
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
 
 
-def get_cars(request): 
+def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if (count == 0): 
+    if (count == 0):
         initiate()
 
     car_models = CarModel.objects.select_related('car_make')
     cars = []
 
-    for car_model in car_models: 
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake":
+            car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 'https://benjn2001-8000.theiadockernext-1- ' + 
-                'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'localhost', 'https://benjn2001-8000.theiadockernext-1- ' +
+            'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://benjn2001-8000.theiadockernext-1- ' + 
-                        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://benjn2001-8000.theiadockernext-1- ' +
+                'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -100,7 +100,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation. ' + 
+        'django.contrib.auth.password_validation. ' +
             'UserAttributeSimilarityValidator',
     },
     {
